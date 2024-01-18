0133c8c47cd1d081f944ef4374ccaf7cb5ab74f4
Ansh718 <ansh007bhutada@gmail.com>
2024-01-18 10:42:29 +0530

    18th

---
+++ January 24/18-01-24.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        long long int result = 0;
+        if (n == 1)
+        {
+            return 1;
+        }
+        if (n == 2)
+        {
+            return 2;
+        }
+        long long int f0 = 1;
+        long long int f1 = 2;
+        for (int i = 2; i < n; i++)
+        {
+            result = f0 + f1;
+            f0 = f1;
+            f1 = result;
+        }
+        return result;
+    }
+};
