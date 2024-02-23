632943a2fbe43f52e250da799d2b66e437871205
Ansh718 <ansh007bhutada@gmail.com>
2024-02-23 12:25:28 +0530

    23

---
+++ February 24/22-02-24.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    int findJudge(int n, vector<vector<int>> &trust)
+    {
+        vector<int> cnt(n + 1);
+        for (vector<int> &t : trust)
+        {
+            --cnt[t[0]];
+            ++cnt[t[1]];
+        }
+        for (int i = 1; i < n + 1; i++)
+        {
+            if (cnt[i] == n - 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
