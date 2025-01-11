c6e9ccb7a2fd1f39df4195680c87ef3fe3ded13f
Ansh718 <ansh007bhutada@gmail.com>
2025-01-11 21:29:17 +0530

    11th

---
+++ 2025/January 25/09-01-2025.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int prefixCount(vector<string> &words, string pref)
+    {
+        int n = 0;
+        n = words.size();
+        int m = 0;
+        m = pref.size();
+        int cnt = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (words[i].substr(0, m) == pref)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
